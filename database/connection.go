f30c4caa991047bd13a5065f6ec202f4475e894f
jevvonn <darmawanb985@gmail.com>
2025-02-18 23:55:39 +0700

    chore: change db name env variable

--- database/connection.go
+++ database/connection.go
@@ -15,9 +15,10 @@ func ConnectDatabase() {
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=gorm port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbName, port)
 
 	database, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
